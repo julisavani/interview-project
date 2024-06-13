c167740e188c6f79a58c5df16851a90c79b26c0d
julisavani <julisavani44@gmail.com>
2024-06-13 19:19:21 +0530

    first commit

--- app/Http/Filters/V1/QueryFilter.php
+++ app/Http/Filters/V1/QueryFilter.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 abstract class QueryFilter {
     protected $builder;
     protected $request;
-    protected $sortable = [];
+   
 
     public function __construct(Request $request)
     {
